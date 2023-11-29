602084583a2182e8eb9a03dc809b0ecb99136d45
Pierre Fervel <pierre.fervel@gmail.com>
2023-11-29 21:55:02 +0100

    INITIAL STATE AFTER PART 1

--- scripts/ground.gd
+++ scripts/ground.gd
@@ -1,21 +1,12 @@
 extends Node2D
 
 
-@onready var lvl_counter = $UiContainer/VBoxContainer/LvlCounter
-@onready var progress_bar = $UiContainer/VBoxContainer/ProgressBar
-
 @onready var ennemy_skeleton = preload("res://scenes/skeleton.tscn")
 
 var has_spawned_skeleton = false
 
 
 func _process(_delta):
-	lvl_counter.text = "Niveau: " + str(Globals.get_player_lvl())
-	
-	progress_bar.max_value = Globals.get_next_lvl_milestone()
-	progress_bar.min_value = Globals.get_previous_lvl_milestone()
-	progress_bar.value = Globals.get_player_XP()
-	
 	if !has_spawned_skeleton:
 		has_spawned_skeleton = true
 		spawn_skeleton()
